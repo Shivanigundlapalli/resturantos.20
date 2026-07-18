7b4e9d6cfa3ff5b5ecf2d38b47c0290f15759e59
Your Name <your_email@gmail.com>
2026-07-18 12:54:39 +0530

    Add startup crash debugger

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,18 @@
-// @ts-nocheck
-import { app } from '../server';
+import express from 'express';
 
-export default app;
+const wrapperApp = express();
+
+wrapperApp.use(async (req, res, next) => {
+  try {
+    const { app } = await import('../server');
+    app(req, res, next);
+  } catch (e: any) {
+    res.status(500).json({
+      error: "Startup Crash",
+      message: e.message,
+      stack: e.stack
+    });
+  }
+});
+
+export default wrapperApp;
