b1ffe7e102ae74edf9a059a8e13c68f41b4220c9
Your Name <your_email@gmail.com>
2026-07-18 12:55:36 +0530

    Test Vercel deploy

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,3 @@
-import express from 'express';
-
-const wrapperApp = express();
-
-wrapperApp.use(async (req, res, next) => {
-  try {
-    const { app } = await import('../server');
-    app(req, res, next);
-  } catch (e: any) {
-    res.status(500).json({
-      error: "Startup Crash",
-      message: e.message,
-      stack: e.stack
-    });
-  }
-});
-
-export default wrapperApp;
+export default function handler(req: any, res: any) {
+  res.status(200).json({ test: "success" });
+}
